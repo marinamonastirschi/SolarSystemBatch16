5f85bf976dcc9cc15c0564ff8c7e1e8b875afce5
marinamonastirschi <m92.fortuna@gmail.com>
2023-07-09 09:54:55 -0500

    i have added the planet2 and is working fine

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-      SolarSystemBasic Omnax= new SolarSystemBasic();
-      Omnax.Moon="2";
-      Omnax.Sun="1";
-      Omnax.Planets="2";
-      Omnax.Star="4";
+        SolarSystemBasic Omnax = new SolarSystemBasic();
+        Omnax.Moon = "2";
+        Omnax.Sun = "1";
+        Omnax.Planets = "2";
+        Omnax.Star = "4";
 
-      fSun AdamX= new fSun();
-      AdamX.name="poya";
-      AdamX.temp="124545 celsius";
-      AdamX.size="3264565km";
+        fSun AdamX = new fSun();
+        AdamX.name = "poya";
+        AdamX.temp = "124545 celsius";
+        AdamX.size = "3264565km";
+
+        Planet2 Feon = new Planet2();
+        Feon.name = "exo";
+        Feon.color = "white";
+        Feon.temp = "25 celsius";
+        Feon.size = "1245 billion km";
     }
 }
